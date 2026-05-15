5f0892bd8fc39fbdb4424bfe3f1f57267b61c0f1
Nikita Kushwaha <1nikita.kushwaha@gmail.com>
2026-05-15 23:38:58 +0530

    Update 1068. Product Sal Analysis I.sql

--- Easy/1068. Product Sal Analysis I.sql
+++ Easy/1068. Product Sal Analysis I.sql
@@ -1,27 +1,38 @@
-Sales table:
-+---------+------------+------+----------+-------+
-| sale_id | product_id | year | quantity | price |
-+---------+------------+------+----------+-------+ 
-| 1       | 100        | 2008 | 10       | 5000  |
-| 2       | 100        | 2009 | 12       | 5000  |
-| 7       | 200        | 2011 | 15       | 9000  |
-+---------+------------+------+----------+-------+
-Product table:
-+------------+--------------+
-| product_id | product_name |
-+------------+--------------+
-| 100        | Nokia        |
-| 200        | Apple        |
-| 300        | Samsung      |
-+------------+--------------+
-Output: 
-+--------------+-------+-------+
-| product_name | year  | price |
-+--------------+-------+-------+
-| Nokia        | 2008  | 5000  |
-| Nokia        | 2009  | 5000  |
-| Apple        | 2011  | 9000  |
-+--------------+-------+-------+
+-- 1068. Product Sales Analysis I
+
+/*
+Question:
+Write an SQL query to report the product_name, year, and price for each sale_id.
+
+Tables:
+
+Sales
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sale_id     | int     |
+| product_id  | int     |
+| year        | int     |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+
+Product
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+
+product_id is the primary key of Product table.
+sale_id + year is the primary key of Sales table.
+*/
+
+-- Approach:
+-- 1. Join Sales and Product tables using product_id
+-- 2. Select required columns:
+--    product_name, year, and price
 
 
 SELECT P.product_name,S.year,S.price 
