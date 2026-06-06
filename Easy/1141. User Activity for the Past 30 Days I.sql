e4f9f6d79231588a14812ce7aae86dd84036df95
Nikita Kushwaha <1nikita.kushwaha@gmail.com>
2026-06-06 23:48:25 +0530

    Modify SQL query for user activity date range

--- Easy/1141. User Activity for the Past 30 Days I.sql
+++ Easy/1141. User Activity for the Past 30 Days I.sql
@@ -34,10 +34,9 @@ Return a table containing:
 
 ## query
 
-SELECT
-    activity_date,
+SELECT 
+    activity_date AS day,
     COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY)
-                        AND '2019-07-27'
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date;
